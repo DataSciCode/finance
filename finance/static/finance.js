f708c125648163ee84c5acefac2613c3f832bb22
Nicola Marcacci Rossi <me@nmr.io>
2013-07-03 21:16:42 +0200

    Added disqus

--- finance/static/finance.js
+++ finance/static/finance.js
@@ -9,6 +9,28 @@ financeServices.factory('Stock', function($resource) {
 });
 var financeFilters = angular.module('financeFilters', []);
 
+function loadDisqus(currentPageId) {
+	console.log(currentPageId);
+	if (typeof DISQUS === 'undefined') {
+		console.log("loading");
+		window.disqus_shortname = 'nicksfinance';
+		window.disqus_identifier = currentPageId; 
+		window.disqus_url = "http://finance.nmr.io/#!"+currentPageId; 
+		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+		dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	} else {
+		console.log("resetting");
+		DISQUS.reset({
+			reload: true,
+			config: function() {
+				this.page.identifier = currentPageId,
+				this.page.url = "http://finance.nmr.io/#!"+currentPageId
+			}
+		});
+	}
+}
+
 var financeApp = angular.module('financeApp', ['financeServices', 'financeFilters'], function($routeProvider) {
     $routeProvider.
     when('/', {controller:Snapshots, templateUrl:'/static/home.html'}).
@@ -17,11 +39,19 @@ var financeApp = angular.module('financeApp', ['financeServices', 'financeFilter
     otherwise({redirectTo:'/'});
 });
 
-function Snapshots($scope, Snapshot) {
+var routeChanged = function($location) {
+	return function(event, next, current) { 
+		loadDisqus($location.path());
+	}
+};
+
+function Snapshots($scope, Snapshot, $location) {
+	$scope.$on('$routeChangeSuccess', routeChanged($location));
     $scope.snapshots = Snapshot.query();
 }
 
 function Stocks($scope, Stock, $routeParams, $location) {
+	$scope.$on('$routeChangeSuccess', routeChanged($location));
     $scope.stocks = Stock.query({snapshot: $routeParams.snapshot});
 
     $scope.search = function() {
@@ -29,6 +59,7 @@ function Stocks($scope, Stock, $routeParams, $location) {
     }
 }
 
-function Stock($scope, Stock, $routeParams) {
+function Stock($scope, Stock, $routeParams, $location) {
+	$scope.$on('$routeChangeSuccess', routeChanged($location));
     $scope.stock = Stock.get({snapshot: $routeParams.snapshot, stock: $routeParams.stock});
 }
